32f9ec2c554281ad09b6c505163e393f7d7aecc5
Akankshya25arp <akankshyaarp03@gmail.com>
2025-07-30 19:09:56 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function App() {
+  return (
+
+    <div>
+      
+      <h1>Hello, World!</h1>
+
+      <nav>
+        <ul>
+  <li>Home</li>
+  <li>About Us</li>
+  <li>Settings</li>
+</ul>
+
+      </nav>
+    </div>
+  );
+}
+
+export default App;
